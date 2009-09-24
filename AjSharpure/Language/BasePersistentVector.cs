ada06f7216ba93a263fe79d396078815cc3a632e
ajlopez <webmaster@ajlopez.com>
2009-09-24 21:07:33 +0000

    IIndexedSequence IPersistentVector IReversible BasePersistentVector initial version wo/tests
    git-svn-id: https://ajlisp.googlecode.com/svn/trunk/AjSharpure@89 6013a6a2-0953-0410-b589-0f418efd76aa

---
+++ AjSharpure/Language/BasePersistentVector.cs
@@ -0,0 +1,223 @@
+﻿namespace AjSharpure.Language
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    // TODO implement RandomAccess, Comparable, Streamable (Java names)
+    public class BasePersistentVector : BaseFunction, IPersistentVector, IList
+    {
+        public BasePersistentVector(IPersistentMap metadata)
+            : base(metadata)
+        {
+        }
+
+        protected BasePersistentVector()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Utilities.PrintString(this);
+        }
+
+        #region IPersistentVector Members
+
+        public int Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IPersistentVector AssociateN(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPersistentVector Cons(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IAssociative Members
+
+        public bool ContainsKey(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DictionaryEntry EntryAt(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAssociative Associate(object key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ValueAt(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ValueAt(object key, object notFound)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IPersistentCollection Members
+
+        IPersistentCollection IPersistentCollection.Cons(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPersistentCollection Empty
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Equiv(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region ISequenceable Members
+
+        public ISequence ToSequence()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region ICounted Members
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IPersistentStack Members
+
+        public object Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPersistentStack Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IReversible Members
+
+        public ISequence ReverseSequence()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IList Members
+
+        public int Add(object value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool IsFixedSize
+        {
+            get { return true; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region ICollection Members
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ AjSharpure/Language/IPersistentVector.cs
@@ -0,0 +1,16 @@
+﻿namespace AjSharpure.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IPersistentVector : IAssociative, ISequential, IPersistentStack, IReversible, ICounted
+    {
+        int Length { get; }
+
+        IPersistentVector AssociateN(int index, object value);
+
+        IPersistentVector Cons(object obj);
+    }
+}
