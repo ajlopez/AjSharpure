12eadf4db82dcce8a30f3e61b430849ad754c755
ajlopez <webmaster@ajlopez.com>
2012-05-01 17:07:04 -0300

    Fixing Source Code Analysis on BasePersistentMap.cs

--- Src/AjSharpure/Language/BasePersistentMap.cs
+++ Src/AjSharpure/Language/BasePersistentMap.cs
@@ -18,6 +18,59 @@
         {
         }
 
+        public IPersistentCollection Empty
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsFixedSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ICollection Keys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ICollection Values
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object this[object key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
         public override string ToString()
         {
             return Utilities.PrintString(this);
@@ -32,8 +85,6 @@
 
             throw new NotImplementedException();
         }
-
-        #region IPersistentMap Members
 
         public IPersistentMap Associate(object key, object value)
         {
@@ -50,18 +101,10 @@
             throw new NotImplementedException();
         }
 
-        #endregion
-
-        #region IEnumerable Members
-
         public IEnumerator GetEnumerator()
         {
             throw new NotImplementedException();
         }
-
-        #endregion
-
-        #region IAssociative Members
 
         public bool ContainsKey(object key)
         {
@@ -88,42 +131,15 @@
             throw new NotImplementedException();
         }
 
-        #endregion
-
-        #region IPersistentCollection Members
-
-
-        public IPersistentCollection Empty
-        {
-            get { throw new NotImplementedException(); }
-        }
-
         public bool Equiv(object obj)
         {
             throw new NotImplementedException();
         }
 
-        #endregion
-
-        #region ISequenceable Members
-
         public ISequence ToSequence()
         {
             throw new NotImplementedException();
         }
-
-        #endregion
-
-        #region ICounted Members
-
-        public int Count
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        #endregion
-
-        #region IDictionary Members
 
         public void Add(object key, object value)
         {
@@ -145,71 +161,19 @@
             throw new NotImplementedException();
         }
 
-        public bool IsFixedSize
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public bool IsReadOnly
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public ICollection Keys
-        {
-            get { throw new NotImplementedException(); }
-        }
-
         public void Remove(object key)
         {
             throw new NotImplementedException();
         }
-
-        public ICollection Values
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public object this[object key]
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotSupportedException();
-            }
-        }
-
-        #endregion
-
-        #region ICollection Members
 
         public void CopyTo(Array array, int index)
         {
             throw new NotImplementedException();
         }
 
-        public bool IsSynchronized
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public object SyncRoot
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        #endregion
-
-        #region IEnumerable Members
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
         }
-
-        #endregion
     }
 }
